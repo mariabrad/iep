7c784afaa9431b5e95dd6d6fca2b423806d17c3b
Maria Brad <ms.mariabrad@yahoo.com>
2021-11-02 14:12:36 +0200

    Made some modifications to the classes

--- Headphones/headphones.cpp
+++ Headphones/headphones.cpp
@@ -1,7 +1,7 @@
 #include "headphones.hpp"
 #include <string.h>
 
-Headphones::Headphones(std::string n, float p,float s, float freq, float in){
+Headphones::Headphones(std::string n, float p, float s, float freq, float in){
   name=n;
   price=p;
   sensitivity=s ;
@@ -9,14 +9,19 @@ Headphones::Headphones(std::string n, float p,float s, float freq, float in){
   input_power=in;
 }
 
-Headphones::~Headphones() { 
-    std::cout << "Destruct headphones" << std::endl; }
+Headphones::~Headphones(){ 
+    std::cout << "Destruct headphones" << std::endl; 
+}
 
-std::string Headphones::getName() {return name;}
-float Headphones::getPrice() {return price;}
+std::string Headphones::getName(){ return name;}
+float Headphones::getPrice(){ return price;}
+float Headphones::getSensitivity(){ return sensitivity;}
+float Headphones::getFrequencyResponse(){ return frequency_response;}
+float Headphones::getInputPower(){ return input_power;}
 
-void Headphones::setName(const std::string w) {name=w;}
-void Headphones::setPrice(const float & h) {price = h;}
-
-
+void Headphones::setName(const std::string w){ name=w;}
+void Headphones::setPrice(const float & h){ price = h;}
+void Headphones::setSensitivity(const float & s){ sensitivity= s;}
+void Headphones::setFrequencyResponse(const float & freq){ frequency_response=freq;}
+void Headphones::setInputPower(const float & in){ input_power=in;}
 
--- Headphones/headphones.hpp
+++ Headphones/headphones.hpp
@@ -4,21 +4,29 @@
 
 class Headphones{
     public:
-    Headphones( std::string n, float p,float s, float freq, float in);
+    Headphones(std::string n, float p, float s, float freq, float in);
     virtual ~Headphones();
     virtual void show()=0;
     Headphones(const Headphones&);
     Headphones& operator=(const Headphones&) = delete; 
     Headphones(Headphones&&) = delete; 
     Headphones& operator=(Headphones&&) = delete;
+
     std::string getName();
     float getPrice();
+    float getSensitivity() {return sensitivity;}
+    float getFrequencyResponse() {return frequency_response;}
+    float getInputPower() {return input_power;}
+
     void setName(const std::string  w); 
     void setPrice(const float & h);
+    void setSensitivity(const float & s);
+    void setFrequencyResponse(const float & freq);
+    void setInputPower(const float & in);
 
     protected:
-        float price;
         std::string name;
+        float price;
         float sensitivity;
         float frequency_response;
         float input_power;
